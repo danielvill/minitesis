6aa1e8230ca43dc28692b2a692b61eb422fea8a0
Daniel Villacres <93736800+danielvill@users.noreply.github.com>
2023-12-19 09:08:36 -0600

    Mejora en la parte visual
    Agregue lo que son las fechas para todas las vistas de html y cosas asi y el agregado de fechas que necesitaba el apartado de date

--- modules/product.py
+++ modules/product.py
@@ -1,21 +1,25 @@
 class Product:
-    def __init__(self, nombre ,codigo, precio,cantidad,resultado,total):
+    def __init__(self, nombre ,codigo, precio,cantidad,resultado,total,fecha_p,fecha_co):
         self.nombre = nombre
         self.codigo = codigo
         self.precio = precio
         self.cantidad = cantidad
         self.resultado = resultado
         self.total = total
+        self.fecha_p=fecha_p
+        self.fecha_co=fecha_co
         
 
     def proDBCollection(self):
         return{
             'nombre': self.nombre,
             'codigo': self.codigo,
-            'price': self.precio,
+            'precio': self.precio,
             'cantidad':self.cantidad,
             'resultado':self.resultado,
-            'total':self.total
+            'total':self.total,
+            'fecha_p':self.fecha_p,
+            'fecha_co':self.fecha_co
             
             
         }
