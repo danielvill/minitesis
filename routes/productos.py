a5ee0ce185bbd35bd92b5e2361d6a5eaa4591e7f
Daniel Villacres <93736800+danielvill@users.noreply.github.com>
2024-09-11 14:31:47 -0600

    Agregado de algo olvidado

--- routes/productos.py
+++ routes/productos.py
@@ -8,3 +8,4 @@ producto = Blueprint("producto",__name__)
 # En este apartado tendre que agregar todo lo relacionado a Productos y si es posible que pueda convertir contraer 
 # Un archivo de lo que es csv para que pueda llenar mi base de datos eso seria 
 
+
